147eb1a9fe6d1e572953356061478f58eab23185
unknown <hasyrawi@gmail.com>
2019-07-13 15:42:37 +0800

    Master Data Siswa Fix .

--- application/controllers/Siswa.php
+++ application/controllers/Siswa.php
@@ -1,109 +1,105 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-
-class Siswa extends CI_controller { 
+defined('BASEPATH') or exit('No direct script access allowed');
+class Siswa extends CI_controller
+{
 
     public function __construct()
     {
-            parent::__construct();
-            // Your own constructor code
-            $this->load->model('Siswa_m' ,'siswa');
-            
+        parent::__construct();
+        // Your own constructor code
+        $this->load->model('Siswa_m', 'siswa');
     }
-   public function index(){
+    public function index()
+    {
         $data['title'] = 'Siswa';
         $data['siswa'] = $this->siswa->get_siswa()->result_array();
-            $this->load->view('templates/header',$data);
-            $this->load->view('templates/sidenav');
-            $this->load->view('templates/navbar', $data);
-            $this->load->view('siswa/index');
-            $this->load->view('templates/footer');
-   }
-   // Tampil kan Data view Show 
-   public function show() { 
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidenav');
+        $this->load->view('templates/navbar', $data);
+        $this->load->view('siswa/index');
+        $this->load->view('templates/footer');
+    }
+    // Tampil kan Data view Show 
+    public function show()
+    {
         $data['title'] = 'Siswa';
         $id = $this->input->get('id');
-        
+
         // cegat apa bila id nya tdk di isi ato langsung akses ke link 
-        if($id !== null ){
+        if ($id !== null) {
             $data['siswa'] = $this->siswa->get_siswa_byId($id)->result_array();
-            $this->load->view('templates/header',$data);
+            $this->load->view('templates/header', $data);
             $this->load->view('templates/sidenav');
             $this->load->view('templates/navbar', $data);
-            $this->load->view('siswa/show',$data);
+            $this->load->view('siswa/show', $data);
             $this->load->view('templates/footer');
-        }else { 
+        } else {
             show_404();
         }
+    }
+    // Tampil kan view Tambah 
+    public function TambahSiswa()
+    {
 
-
-   }
-   // Tampil kan view Tambah 
-   public function TambahSiswa(){  
-
-    $data['title'] = 'Tambah Data Siswa';
-        $this->load->view('templates/header',$data);
+        $data['title'] = 'Tambah Data Siswa';
+        $this->load->view('templates/header', $data);
         $this->load->view('templates/sidenav');
         $this->load->view('templates/navbar', $data);
         $this->load->view('siswa/TambahSiswa');
         $this->load->view('templates/footer');
+    }
 
-   }
-
-   public function prosesTambah(){
-       // Tangkap Semua Data form Post
+    public function prosesTambah()
+    {
+        // Tangkap Semua Data form Post
         $siswa = [
-            'nama'      => $this->input->post('nama' , true),
-            'alamat'    => $this->input->post('alamat' , true),
-            'nis'       => $this->input->post('nis' , true),
-            'kelas'     => $this->input->post('kelas' , true ), 
-            'jk'        => $this->input->post('jk' , true),
-            'no_hp'     => $this->input->post('no_hp' , true)
-              
-        ];   
-        $data = $this->siswa->insert_siswa($siswa);
-    
-   }
-   // Controlle Untuk Hapus Data Siswa 
-   // Terima Id dari url dengan Get 
+            'nama'      => $this->input->post('nama', true),
+            'alamat'    => $this->input->post('alamat', true),
+            'nis'       => $this->input->post('nis', true),
+            'kelas'     => $this->input->post('kelas', true),
+            'jk'        => $this->input->post('jk', true),
+            'no_hp'     => $this->input->post('no_hp', true)
 
-   public function hapus_siswa(){
-       $id =  $this->input->get('id');
-       if($id !== null ) {
-        $this->siswa->hapus_siswa($id);
-       }else { 
-           show_404();
-       }
-   }
-   // Tampilkan Menu Edit 
-   public function Edit($id){ 
+        ];
+        $data = $this->siswa->insert_siswa($siswa);
+    }
+    // Controlle Untuk Hapus Data Siswa 
+    // Terima Id dari url dengan Get 
+
+    public function hapus_siswa()
+    {
+        $id =  $this->input->get('id');
+        if ($id !== null) {
+            $this->siswa->hapus_siswa($id);
+        } else {
+            show_404();
+        }
+    }
+    // Tampilkan Menu Edit 
+    public function Edit($id)
+    {
 
         $data['title'] = 'Edit Data Siswa';
         $data['siswa'] = $this->siswa->get_siswa_byId($id)->result_array();
-        $this->load->view('templates/header',$data);
+        $this->load->view('templates/header', $data);
         $this->load->view('templates/sidenav');
         $this->load->view('templates/navbar', $data);
         $this->load->view('siswa/edit');
         $this->load->view('templates/footer');
-
-   }
-   //Proses update Siswa
-   public function update(){
+    }
+    //Proses update Siswa
+    public function update()
+    {
 
         $siswa = [
-            'nama'      => $this->input->post('nama' , true),
-            'alamat'    => $this->input->post('alamat' , true),
-            'nis'       => $this->input->post('nis' , true),
-            'kelas'     => $this->input->post('kelas' , true ), 
-            'jk'        => $this->input->post('jk' , true),
-            'no_hp'     => $this->input->post('no_hp' , true)
-            
-        ];   
-        
+            'nama'      => $this->input->post('nama', true),
+            'alamat'    => $this->input->post('alamat', true),
+            'nis'       => $this->input->post('nis', true),
+            'kelas'     => $this->input->post('kelas', true),
+            'jk'        => $this->input->post('jk', true),
+            'no_hp'     => $this->input->post('no_hp', true)
+
+        ];
         $data = $this->siswa->update_siswa($siswa);
-
-   }
-
-
+    }
 }
--- application/models/Siswa_m.php
+++ application/models/Siswa_m.php
@@ -1,86 +1,84 @@
-<?php 
+<?php
 
 /*
 Class Model untuk Siswa 
 Sesuaikan Model Dengan field Database nya 
-*/ 
-defined('BASEPATH') OR exit('No direct script access allowed');
-class Siswa_m extends CI_Model {
+*/
+defined('BASEPATH') or exit('No direct script access allowed');
+class Siswa_m extends CI_Model
+{
 
-        // Select Data Siswa 
-        public function get_siswa(){
-            $data  = $this->db->get('t_siswa');
-            return $data;
+    // Select Data Siswa 
+    public function get_siswa()
+    {
+        $data  = $this->db->get('t_siswa');
+        return $data;
+    }
+    // Ambil Data Siswa berdasarkan id 
+    // Terima $id dari controller Siswa
+    public function get_siswa_byId($id)
+    {
+
+        $data = $this->db->get_where('t_siswa', ['siswa_id' => $id]);
+
+        return $data;
+    }
+    // Tambah Data Siswa
+    // var $siswa di terima dari contriller siswa 
+    // Menerima Data Dari form inputan 
+    public function insert_siswa($siswa)
+    {
+
+        $q = $this->db->insert('t_siswa', $siswa);
+        if ($q === true) {
+            redirect('siswa', 'refresh');
         }
-        // Ambil Data Siswa berdasarkan id 
-        // Terima $id dari controller Siswa
-        public function get_siswa_byId($id){
-            
-            $data = $this->db->get_where('t_siswa' , ['siswa_id' => $id]);
-            
-            return $data ;
-            
-        }
-        // Tambah Data Siswa
-        // var $siswa di terima dari contriller siswa 
-        // Menerima Data Dari form inputan 
-        public function insert_siswa($siswa) {
+    }
 
-            $q = $this->db->insert('t_siswa', $siswa);
-            if($q === true ) {  
-                redirect('siswa','refresh');
-            }          
-        }
+    // Update Data Siswa 
+    // Var $siswa Di ambil Dri Controller update 
+    // dengan method update_siswa
 
-        // Update Data Siswa 
-        // Var $siswa Di ambil Dri Controller update 
-        // dengan method update_siswa
+    public function update_siswa($siswa)
+    {
 
-        public function update_siswa($siswa){
+        // Cegat Akses Url Dengan parameter nulll 
 
-            // Cegat Akses Url Dengan parameter nulll 
+        $url = $this->uri->segment(3);
 
-            $url = $this->uri->segment(3);
-
-            if($url !== null ){
-                $q = $this->db->update('t_siswa', $siswa);
-                if($q === true) { 
-                    redirect('siswa','refresh');
-                }else { 
-                    echo "gagal" ;
-                }
-
+        if ($url !== null) {
+            $q = $this->db->update('t_siswa', $siswa);
+            if ($q === true) {
+                redirect('siswa', 'refresh');
             } else {
-                show_404();
+                echo "gagal";
             }
-
-           
-            
-
+        } else {
+            show_404();
         }
+    }
 
 
 
-        //Hapus Data Siswa 
-        // var $id di dapat dari Controller Siswa 
-        // dengan menerima Parameter id mengunakan 
-        // get url id '?id=$id <= yang tertuju '
-         public function hapus_siswa($id) { 
-         $q = $this->db->delete('t_siswa' , ['siswa_id' => $id]);
-            if($q === true ) { 
-                
-                redirect('siswa','refresh');
-            }
+    //Hapus Data Siswa 
+    // var $id di dapat dari Controller Siswa 
+    // dengan menerima Parameter id mengunakan 
+    // get url id '?id=$id <= yang tertuju '
+    public function hapus_siswa($id)
+    {
+        $q = $this->db->delete('t_siswa', ['siswa_id' => $id]);
+        if ($q === true) {
 
-         }
-         // Hitung Jumalah Siswa keselurahan Dalam Tabel 
-         public function hitung_siswa() { 
-            
-            $data = $this->db->count_all('t_siswa');
-            return  $data ; 
+            redirect('siswa', 'refresh');
+        }
+    }
+    // Hitung Jumalah Siswa keselurahan Dalam Tabel 
+    public function hitung_siswa()
+    {
 
-         }
-
+        $data = $this->db->count_all('t_siswa');
+        return  $data;
+    }
 }
 
 /* End of file Siswa_m.php */
